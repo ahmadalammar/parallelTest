122e0ca34112c77c88a8ede6ca20ba2314a2e3aa
“Alammar” <ahmad1costi@gmail.com>
2022-12-11 12:53:32 +0800

    init commit

---
+++ src/test/java/com/alammar/parallelTest/ParallelTestOne.java
@@ -0,0 +1,25 @@
+package com.alammar.parallelTest;
+
+import org.junit.jupiter.api.Test;
+
+//@Execution(CONCURRENT)
+class ParallelTestOne {
+
+    @Test
+    void oneSecondTest() throws InterruptedException {
+        System.out.println("oneSecondTest() name  =>  " + Thread.currentThread().getName());
+        Thread.sleep(1000);
+    }
+
+    @Test
+    void twoSecondTest() throws InterruptedException {
+        System.out.println("twoSecondTest() name => " + Thread.currentThread().getName());
+        Thread.sleep(2000);
+    }
+
+    @Test
+    void threeSecondTest() throws InterruptedException {
+        System.out.println("threeSecondTest() name => " + Thread.currentThread().getName());
+        Thread.sleep(3000);
+    }
+}
